05ddf54fe86d05485d65dee6145c8ac440df691c
John Bond <johnbond@live.nl>
2021-02-22 19:18:35 +0100

    CMS

---
+++ header.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require 'Controller/UserController.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=9" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CMS</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Sign in</title>
+</head>
