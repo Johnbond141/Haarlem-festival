4cf3d6a88a0d7a07025787e301884dc5a2e84eb8
Wout de Roy van Zuydewijn <woutrvz@gmail.com>
2021-03-01 14:36:03 +0100

    Jazz header and part of page itself

---
+++ jazz.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/jazzcss.css">
+    <title>Jazz homepage</title>
+</head>
+
+<body>
+    <nav>
+    </nav>
+    <header class="header">
+        <h1 class="eventhead">Jazz</h1>
+        <h2 class="slogan">Jazz it up a little!</h2>
+    </header>
+    <section class="container">
+        <article class="box1"></article>
+        <article class="box2">
+            The greatest jazz artisti will be performing
+            at the Haarlem Festival. There will be shown a variety
+            of different jazz styles. Order your tickets
+            while you still can and we hope to see you there.
+            <br><br><br>
+            For more information about the performing
+            artist
+            you can scroll down and go to the artist page.
+        </article>
+    </section>
+
+</body>
+
+</html>
