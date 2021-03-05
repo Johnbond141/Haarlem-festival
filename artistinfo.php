881e84597fe95f183d02bac90ea3e9717c4df47f
Wout de Roy van Zuydewijn <woutrvz@gmail.com>
2021-03-05 18:34:43 +0100

    artist info page added (WIP)

---
+++ artistinfo.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/jazzcss.css">
+    <title>Artist Information</title>
+</head>
+
+<body>
+    <header class="header">
+        <h1 class="eventhead">Jazz Artists</h1>
+        <h2 class="slogan">Jazz it up a little!</h2>
+    </header>
+    <section class="agenda">
+        <article class="title-wrapper">
+            <h1>Festival Agenda</h1>
+            <button>Thursday</button>
+        </article>
+        <article class="info-wrapper">
+            <h2>Gumbo Kings</h2>
+            <p>Patronaat, Main Hall</p>
+            <p>18:00 - 19:00, July 26th</p>
+        </article>
+    </section>
+</body>
+
+</html>
