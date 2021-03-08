27a0dda1a1b769c360be3ba6001d813506c9c4f8
Nicky Garcia Jorge <portonicky2@gmail.com>
2021-03-08 20:41:54 +0100

    WIP: alle restaurantinfo , nog geen css

--- Mainpage-foodevent.php
+++ Mainpage-foodevent.php
@@ -4,6 +4,11 @@ include_once 'header.php';
 <body>
 
 <body>
+
+<?php
+include_once 'navBar.php';
+?>
+
 <section class="img-column-foodpage">
     <img src="img/FoodEvent-banner.png" alt="Foodevent" style="width: 100%" class="image"> 
 </section>
@@ -12,117 +17,93 @@ include_once 'header.php';
 <h2 class="Foodpage-subtitle">Good food is all you need</h2>
 
 <ol>
-	<li class="Restaurant1">
+	<li class="Restaurants1">
 	Restaurant MR. & Mrs.
-		<p class="tien">
+		<p class="Restaurants">
 	    	Lange Veerstraat 4
-		    <img src="Haarlem-festival/img/foodpage-Shrimp.png "  
+		    <img src="img/Restaurant1.png "  
 		    title="Restaurant MR. & Mrs."
    		    alt= "A picture of a shrimp"
 		    class= "Restaurantfoto">
             Dutch, Fish and seafood, European
 	    </p>
 	</li>
-	<li class="Restaurant2">
+	<li class="Restaurants2">
 	Ratatouille
-		<p class="tien">
+		<p class="Restaurants">
             Spaarne 96
-			<img src="img/Schieten.png"
+			<img src="img/Restaurant2.png"
 		    title="Ratatouille"
-			alt= "A picture of a goalkeeper shooting the ball"
+			alt= "A picture of ratatouille"
 			class= "Restaurantfoto">
             French, Fish and seafood, European
 		</p>
 	</li>
-				<li class="ClubsA">
-					SV Hoofddorp
-					<p class="tien">
-						SV Hoofddorp is een voetbalclub van een hoog niveau. De club bied zoals elke andere club iedereen de mogelijkheid om lekker te sporten, maar het extra pluspuntje van deze club is het hoge niveau waarop er word gevoetbald en de samenwerkingen met andere nog betere clubs.
-						<img src="img/Voetbal.png"
-						title="Voetbal"
-						alt= "A picture of a soccer ball on the pitch"
-						class= "sportfoto"
-						>
-						
-						<a href= https://www.svhoofddorp.nl/ class="link">
-						Website SV Hoofddorp.
-						</a>	
-					</p>
-				</li>
-				<li class="ClubsB">
-					Club Kops
-					<p class="tien">
-						Club Kops is een vechtsportschool die eigenlijk drie verschillende mogelijkheden aan mensen aanbied. Zo worden er lessen gegeven in conditioneel kickboksen en judo, maar ook worden er lessen gegeven in ouder-kind judo voor ouders die zelf ook graag meedoen met hun kind.
-						<img src="img/Vechtsport.png"
-						title="Vechtsport"
-						alt= "A picture of a silouette kicking"
-						class= "sportfoto"
-						>
-						
-						<a href= https://clubkops.nl/ class="link">
-						Website Club Kops.
-						</a>	
-					</p>
-				</li>
-				<li class="ClubsA">
-					Budo Vereniging Dojo Saikoro
-					<p class="tien">
-						Budo Vereniging Dojo Saikoro is een vechtsportschool waar lessen worden gegeven in karate en jiu jitsu. Naast het leren van zelfverdediging, wordt er ook een grote focus gelegd op het omgaan met de medeleerlingen en het opbouwen van zelfvertrouwen.
-						<img src="img/JIU.png"
-						title="Jiu Jitsu"
-						alt= "A picture of a Jiu Jitsu move"
-						class= "sportfoto"
-						>
-						
-						<a href= http://www.saikoro.nl/ class="link">
-						Website Budo Vereniging Dojo Saikoro.
-						</a>	
-					</p>
-				</li>
-				<li class="ClubsB">
-					Overbos
-					<p class="tien">
-						Overbos is de derde voetbalclub in de buurt en heeft net al de andere twee club als het voornaamste doel om kinderen gewoon te kunnen laten voetballen. En zoals de andere twee clubs is het daar ook altijd het gezellig en word er ook een focus gelegd op teamgeest en plezier hebben met je teamgenoten.
-						<img src="img/Voetbalzak.png"
-						title="Zak met voetballen erin"
-						alt= "A picture of a couple of soccer balls"
-						class= "sportfoto"
-						>
-						
-						<a href= https://www.overbos.nl/ class="link">
-						Website Overbos.
-						</a>	
-					</p>
-				</li>
-				<li class="ClubsA">
-					MHC De Reigers
-					<p class="tien">
-						MHC De Reigers is de enige hockeyclub in Hoofddorp en is dus de enige optie voor mensen die niet buiten Hoofddorp willen gaan. De club heeft vele jeugdteams en een hoop senioren teams, die allemaal kunnen genieten van de evenementen die worden georganiseerd.
-						<img src="img/Hockey.png"
-						title="Hockey"
-						alt= "A picture of 2 girls playing hockey"
-						class= "sportfoto"
-						>
-						
-						<a href= https://www.mhcdereigers.nl/site/Default.asp class="link">
-						Website MHC De Reigers.
-						</a>	
-					</p>
-				</li>
-				<li class="ClubsB">
-					Sportpark Pioniers
-					<p class="tien">
-						Sportpark Pioniers is een honknalclub geveestigd in Hoofddorp. De club bied honkbalmogelijkheden voor jong en oud, en natuurlijk ook voor jongen en voor meiden. De club honkbalt al tientallen jaren op hoog niveau, er is zelfs al meerdere keren meegedaan aan het Ek Honk- en Softbal.
-						<img src="img/Honkbal.png"
-						title="Honkbal"
-						alt= "A picture of a baseballplayer running after hitting the ball"
-						class= "sportfoto"
-						>
-						
-						<a href= https://www.hoofddorp-pioniers.nl/ class="link">
-						Website Sportpark Pioniers.
-						</a>
-					</p>
-				</li>
+	<li class="Restaurants1">
+		Restaurant ML
+		<p class="Restaurants">
+			Kleine Houtstraat 70
+			<img src="img/Restaurant3.png"
+			title="Restaurant ML"
+			alt= "A picture of seafood"
+			class= "restaurantfoto">
+			Dutch, fish and seafood, European
+		</p>
+	</li>
+	<li class="Restaurants2">
+	Restaurant Fris
+		<p class="Restaurants">
+            Twijnderslaan 7
+			<img src="img/Restaurant4.png"
+		    title="Restaurant Fris"
+			alt= "A picture of a steak"
+			class= "Restaurantfoto">
+            Dutch, European
+		</p>
+	</li>
+	<li class="Restaurants1">
+		Specktakel
+		<p class="Restaurants">
+			Spekstraat 4
+			<img src="img/Restaurant5.png"
+			title="Specktakel"
+			alt= "A picture of an asian dish"
+			class= "restaurantfoto">
+			European, International, Asian
+		</p>
+	</li>
+	<li class="Restaurants2">
+	Grand Cafe Brinkman
+		<p class="Restaurants">
+            Grote markt 13
+			<img src="img/Restaurant6.png"
+		    title="Grand Cafe Brinkman"
+			alt= "A picture of spaghetti"
+			class= "Restaurantfoto">
+            Dutch, European, Modern
+		</p>
+	</li>
+	<li class="Restaurants1">
+	Urban Frenchy Bistro Toujours
+		<p class="Restaurants">
+			Oude groenmarkt 10-12
+			<img src="img/Restaurant7.png"
+			title="Urban Frenchy Bistro Toujours"
+			alt= "A picture of salmon"
+			class= "restaurantfoto">
+			Dutch, fish and seafood, European
+		</p>
+	</li>
+	<li class="Restaurants2">
+	The golden bull
+		<p class="Restaurants">
+            Zijlstraat 39
+			<img src="img/Restaurant8.png"
+		    title="The golden bull"
+			alt= "A picture of potatoes and steak"
+			class= "Restaurantfoto">
+            Steakhouse, Argentinian, European
+		</p>
+	</li>
 
 </body>
