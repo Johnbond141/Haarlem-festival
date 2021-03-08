be836f1402301cc48f0605c6eb1b2502e48f7a81
Nicky Garcia Jorge <portonicky2@gmail.com>
2021-03-08 14:17:49 +0100

    WIP: Food homepage

---
+++ Mainpage-foodevent.php
@@ -0,0 +1,132 @@
+<?php
+include_once 'header.php';
+?>
+<body>
+
+<body>
+<section class="img-column-foodpage">
+    <img src="img/FoodEvent-banner.png" alt="Foodevent" style="width: 100%" class="image"> 
+</section>
+
+<h1 class="Foodpage-title">Food Event</h1>
+<h2 class="Foodpage-subtitle">Good food is all you need</h2>
+
+<ol>
+	<li class="ClubsA">
+	Restaurant MR. & Mrs.
+		<p class="tien">
+	    	Lange Veerstraat 4
+		    <img src="img/foodpage-Shrimp.png "  
+		    title="Restaurant MR. & Mrs."
+   		    alt= "A picture of a shrimp"
+		    class= "Restaurantfoto">
+            Dutch, Fish and seafood, European
+	    </p>
+	</li>
+				<li class="ClubsB">
+					VV Uno
+					<p class="tien">
+						VV Uno is een voetbalclub die is ontstaan omdat er te weinig plek was voor kinderen bij andere clubs. De club wil iedereen de kans geven om te kunnen sporten.
+						<img src="img/Schieten.png"
+						title="Voetbal"
+						alt= "A picture of a goalkeeper shooting the ball"
+						class= "sportfoto"
+						>
+						
+						<a href= https://www.vvuno.nl// class="link">
+						Website VV Uno.
+						</a>	
+					</p>
+				</li>
+				<li class="ClubsA">
+					SV Hoofddorp
+					<p class="tien">
+						SV Hoofddorp is een voetbalclub van een hoog niveau. De club bied zoals elke andere club iedereen de mogelijkheid om lekker te sporten, maar het extra pluspuntje van deze club is het hoge niveau waarop er word gevoetbald en de samenwerkingen met andere nog betere clubs.
+						<img src="img/Voetbal.png"
+						title="Voetbal"
+						alt= "A picture of a soccer ball on the pitch"
+						class= "sportfoto"
+						>
+						
+						<a href= https://www.svhoofddorp.nl/ class="link">
+						Website SV Hoofddorp.
+						</a>	
+					</p>
+				</li>
+				<li class="ClubsB">
+					Club Kops
+					<p class="tien">
+						Club Kops is een vechtsportschool die eigenlijk drie verschillende mogelijkheden aan mensen aanbied. Zo worden er lessen gegeven in conditioneel kickboksen en judo, maar ook worden er lessen gegeven in ouder-kind judo voor ouders die zelf ook graag meedoen met hun kind.
+						<img src="img/Vechtsport.png"
+						title="Vechtsport"
+						alt= "A picture of a silouette kicking"
+						class= "sportfoto"
+						>
+						
+						<a href= https://clubkops.nl/ class="link">
+						Website Club Kops.
+						</a>	
+					</p>
+				</li>
+				<li class="ClubsA">
+					Budo Vereniging Dojo Saikoro
+					<p class="tien">
+						Budo Vereniging Dojo Saikoro is een vechtsportschool waar lessen worden gegeven in karate en jiu jitsu. Naast het leren van zelfverdediging, wordt er ook een grote focus gelegd op het omgaan met de medeleerlingen en het opbouwen van zelfvertrouwen.
+						<img src="img/JIU.png"
+						title="Jiu Jitsu"
+						alt= "A picture of a Jiu Jitsu move"
+						class= "sportfoto"
+						>
+						
+						<a href= http://www.saikoro.nl/ class="link">
+						Website Budo Vereniging Dojo Saikoro.
+						</a>	
+					</p>
+				</li>
+				<li class="ClubsB">
+					Overbos
+					<p class="tien">
+						Overbos is de derde voetbalclub in de buurt en heeft net al de andere twee club als het voornaamste doel om kinderen gewoon te kunnen laten voetballen. En zoals de andere twee clubs is het daar ook altijd het gezellig en word er ook een focus gelegd op teamgeest en plezier hebben met je teamgenoten.
+						<img src="img/Voetbalzak.png"
+						title="Zak met voetballen erin"
+						alt= "A picture of a couple of soccer balls"
+						class= "sportfoto"
+						>
+						
+						<a href= https://www.overbos.nl/ class="link">
+						Website Overbos.
+						</a>	
+					</p>
+				</li>
+				<li class="ClubsA">
+					MHC De Reigers
+					<p class="tien">
+						MHC De Reigers is de enige hockeyclub in Hoofddorp en is dus de enige optie voor mensen die niet buiten Hoofddorp willen gaan. De club heeft vele jeugdteams en een hoop senioren teams, die allemaal kunnen genieten van de evenementen die worden georganiseerd.
+						<img src="img/Hockey.png"
+						title="Hockey"
+						alt= "A picture of 2 girls playing hockey"
+						class= "sportfoto"
+						>
+						
+						<a href= https://www.mhcdereigers.nl/site/Default.asp class="link">
+						Website MHC De Reigers.
+						</a>	
+					</p>
+				</li>
+				<li class="ClubsB">
+					Sportpark Pioniers
+					<p class="tien">
+						Sportpark Pioniers is een honknalclub geveestigd in Hoofddorp. De club bied honkbalmogelijkheden voor jong en oud, en natuurlijk ook voor jongen en voor meiden. De club honkbalt al tientallen jaren op hoog niveau, er is zelfs al meerdere keren meegedaan aan het Ek Honk- en Softbal.
+						<img src="img/Honkbal.png"
+						title="Honkbal"
+						alt= "A picture of a baseballplayer running after hitting the ball"
+						class= "sportfoto"
+						>
+						
+						<a href= https://www.hoofddorp-pioniers.nl/ class="link">
+						Website Sportpark Pioniers.
+						</a>
+					</p>
+				</li>
+
+</body>
