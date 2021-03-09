1bb2cc67796af4ccd57e06211bdc761ab12d09d5
Wout de Roy van Zuydewijn <woutrvz@gmail.com>
2021-03-09 16:28:58 +0100

    WIP: added footer to jazz page and fixed the footer to bottom
    Fixed footer to bottom and added navbar to jazz page also added includes folder for better overview

---
+++ includes/footer.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="../css/footercss.css"> -->
+    <title>Footer</title>
+</head>
+
+<body>
+
+    <section class="wrapperfooter">
+        <img class="logofooter" src="img/Logo HF.png" alt="logo of haarlem festival">
+        <h3>Menu</h3>
+        <article class="footermenu">
+            <ul class="section1">
+                <li>Home</li>
+                <li>About us</li>
+                <li>Contact us</li>
+                <li>Tickets</li>
+            </ul>
+            <ul class="section2">
+                <li>Artist</li>
+                <li>Create a program</li>
+                <li>Jazz</li>
+                <li>Food</li>
+            </ul>
+            <ul class="section3">
+                <li>Dance</li>
+            </ul>
+        </article>
+
+        <article class="linefooter"></article>
+        <article class="copyright">
+            <p><span>&#169;</span>Copyright. All right reserved.</p>
+        </article>
+    </section>
+
+</body>
+
+</html>
--- jazz.php
+++ jazz.php
@@ -1,7 +1,9 @@
+<?php
+include("includes/navBar.php");
+?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 
 <head>
     <meta charset="UTF-8">
@@ -12,48 +14,52 @@
 </head>
 
 <body>
-    <nav>
-    </nav>
-    <header class="header">
-        <h1 class="eventhead">Jazz</h1>
-        <h2 class="slogan">Jazz it up a little!</h2>
-    </header>
-    <section class="container">
-        <article class="box1"></article>
-        <article class="box2">
-            The greatest jazz artists will be performing
-            at the Haarlem Festival. There will be shown a variety
-            of different jazz styles. Order your tickets
-            while you still can and we hope to see you there.
-            <br><br><br>
-            For more information about the performing
-            artist
-            you can scroll down and go to the artist page.
-            <form action="">
-                <input type="submit" class="buyjazz" value="Buy tickets"><i class="arrow right"></i></input>
-            </form>
-        </article>
-    </section>
-    <section class="wrapper">
-        <section class="container2">
-            <article class="box3">
-                Some great artist will be performing on the Haarlem festival.
-                Such as The Nordians and Ntjam Rosie.
-                Make sure to not miss out on any of them.
-                If you need more information about the performing
-                times and the location before buying your tickets
-                you can find a button below.
-
+    <section class="page-container">
+        <header class="header">
+            <h1 class="eventhead">Jazz</h1>
+            <h2 class="slogan">Jazz it up a little!</h2>
+        </header>
+        <section class="container">
+            <article class="box1"></article>
+            <article class="box2">
+                The greatest jazz artists will be performing
+                at the Haarlem Festival. There will be shown a variety
+                of different jazz styles. Order your tickets
+                while you still can and we hope to see you there.
+                <br><br><br>
+                For more information about the performing
+                artist
+                you can scroll down and go to the artist page.
                 <form action="">
-                    <input type="submit" class="artistinfo" name="artistinfo" value="Artist Information">
+                    <input type="submit" class="buyjazz" value="Buy tickets"><i class="arrow right"></i></input>
                 </form>
             </article>
-            <article class="box4">
-                <img src="img/jazz2.png"  width="500px" height="auto" alt="">
-            </article>
+        </section>
+        
+        <section class="wrapper">
+            <section class="container2">
+                <article class="box3">
+                    Some great artist will be performing on the Haarlem festival.
+                    Such as The Nordians and Ntjam Rosie.
+                    Make sure to not miss out on any of them.
+                    If you need more information about the performing
+                    times and the location before buying your tickets
+                    you can find a button below.
+                    <form action="">
+                        <input type="submit" class="artistinfo" name="artistinfo" value="Artist Information">
+                    </form>
+                </article>
+                <article class="box4">
+                    <img src="img/jazz2.png" width="500px" height="auto" alt="">
+                </article>
+            </section>
         </section>
     </section>
-
 </body>
 
 </html>
+
+<?php
+include("includes/footer.php");
+
+?>
