9052eb567875c9cc388a8acee944ad0943db942d
Wout de Roy van Zuydewijn <woutrvz@gmail.com>
2021-03-10 17:09:54 +0100

    Finished: Footer

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="../css/footercss.css"> -->
+    <link rel="stylesheet" href="../css/jazzcss.css">
     <title>Footer</title>
 </head>
 
@@ -13,27 +13,58 @@
 
     <section class="wrapperfooter">
         <img class="logofooter" src="img/Logo HF.png" alt="logo of haarlem festival">
-        <h3>Menu</h3>
+        <article class="menutag">
+            <h3>Menu</h3>
+        </article>
         <article class="footermenu">
             <ul class="section1">
-                <li>Home</li>
-                <li>About us</li>
-                <li>Contact us</li>
-                <li>Tickets</li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Contact us</a></li>
+                <li><a href="#">Tickets</a></li>
             </ul>
             <ul class="section2">
-                <li>Artist</li>
-                <li>Create a program</li>
-                <li>Jazz</li>
-                <li>Food</li>
+                <li><a href="#">Artists</a></li>
+                <li><a href="#">Create a program</a></li>
+                <li><a href="#">Jazz</a></li>
+                <li><a href="#">Food</a></li>
             </ul>
             <ul class="section3">
-                <li>Dance</li>
+                <li><a href="#">Dance</a></li>
             </ul>
         </article>
-
+        <article class="latestposttag">
+            <h3>Latest Posts</h3>
+        </article>
+        <article class="latestposts">
+            <section class="insta">
+                <a href="https://www.instagram.com"><img src="img/instagramlogo.png" alt="picture of instagram logo"></a>
+                <p>Hardwell on Haarlem Festival
+                    <br><span style="color: black;">by</span> Haarlem Festival
+                </p>
+            </section>
+            <section class="facebook">
+                <a href="https://www.facebook.com"><img src="img/facebooklogo.png" alt="picture of facebook logo"></a>
+                <p><span style="color: black;">@HaarlemFestival</span> Buy your tickets
+                    <br>now for the next festival</p>
+            </section>
+        </article>
+        <article class="stayconnectedtag">
+            <h3>Stay Connected</h3>
+        </article>
+        <article class="stayconnected">
+            <form action="">
+                <input class="email" type="text" name="email" placeholder="Email Address...">
+                <input class="signup" type="submit" name="signup" value="Sign-Up">
+            </form>
+        </article>
         <article class="linefooter"></article>
         <article class="copyright">
+        <article class="copyrightlogos">
+        <a href="https://www.facebook.com"><img src="img/facebook.png" alt="picture of facebook logo"></a>
+        <a href="https://www.youtube.com"><img src="img/youtube.png" alt="image of youtube logo"></a>
+        <a href="https://www.instagram.com"><img src="img/instagram.png" alt="image of instagram logo"></a>
+        </article>
             <p><span>&#169;</span>Copyright. All right reserved.</p>
         </article>
     </section>
