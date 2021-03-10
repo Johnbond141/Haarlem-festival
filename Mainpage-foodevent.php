28932b68134684197d16e6704950e2f8bc893a67
Nicky Garcia Jorge <619307@student.inholland.nl>
2021-03-11 00:00:26 +0100

    WIP: Food page incl navbar,banner, restaurants

--- Mainpage-foodevent.php
+++ Mainpage-foodevent.php
@@ -6,99 +6,101 @@
 	<body>
 
 		<section class="img-column-foodpage">
-			<img src="img/FoodEvent-banner.png" alt="Foodevent" style="width: 100%" class="image"> 
+			
 		</section>
 
-		<h1 class="Foodpage-title">Food Event</h1>
-		<h2 class="Foodpage-subtitle">Good food is all you need</h2>
+		<header class="indexHeader">
+		<h1 class="indexTitle">Food Event</h1>
+		<h2 class="indexSlogan">Good food is all you need</h2>
+		</header>
 
 		<ol>
 			<li class="Restaurants1">
 			Restaurant MR. & Mrs.
 				<p class="Restaurants">
-					Lange Veerstraat 4
+					<h4 class="Adress">Lange Veerstraat 4</h4>
+					Dutch, Fish and seafood, European
 					<img src="img/Restaurant1.png "  
 					title="Restaurant MR. & Mrs."
 					alt= "A picture of a shrimp"
-					class= "Restaurantfoto">
-					Dutch, Fish and seafood, European
+					class= "Restaurantfoto1">
 				</p>
 			</li>
 			<li class="Restaurants2">
 			Ratatouille
 				<p class="Restaurants">
-					Spaarne 96
+					<h4 class="Adress">Spaarne 96</h4>
+					French, Fish and seafood, European
 					<img src="img/Restaurant2.png"
 					title="Ratatouille"
 					alt= "A picture of ratatouille"
-					class= "Restaurantfoto">
-					French, Fish and seafood, European
+					class= "Restaurantfoto2">
 				</p>
 			</li>
 			<li class="Restaurants1">
-				Restaurant ML
+			Restaurant ML
 				<p class="Restaurants">
-					Kleine Houtstraat 70
+					<h4 class="Adress">Kleine Houtstraat 70</h4>
+					Dutch, fish and seafood, European
 					<img src="img/Restaurant3.png"
 					title="Restaurant ML"
 					alt= "A picture of seafood"
-					class= "restaurantfoto">
-					Dutch, fish and seafood, European
+					class= "restaurantfoto1">
 				</p>
 			</li>
 			<li class="Restaurants2">
 			Restaurant Fris
 				<p class="Restaurants">
-					Twijnderslaan 7
+					<h4 class="Adress">Twijnderslaan 7</h4>				
+					Dutch, European
 					<img src="img/Restaurant4.png"
 					title="Restaurant Fris"
 					alt= "A picture of a steak"
-					class= "Restaurantfoto">
-					Dutch, European
+					class= "Restaurantfoto2">
 				</p>
 			</li>
 			<li class="Restaurants1">
 				Specktakel
 				<p class="Restaurants">
-					Spekstraat 4
+					<h4 class="Adress">Spekstraat 4</h4>
+					European, International, Asian
 					<img src="img/Restaurant5.png"
 					title="Specktakel"
 					alt= "A picture of an asian dish"
-					class= "restaurantfoto">
-					European, International, Asian
+					class= "restaurantfoto1">
 				</p>
 			</li>
 			<li class="Restaurants2">
 			Grand Cafe Brinkman
 				<p class="Restaurants">
-					Grote markt 13
+					<h4 class="Adress">Grote markt 13</h4>
+					Dutch, European, Modern
 					<img src="img/Restaurant6.png"
 					title="Grand Cafe Brinkman"
 					alt= "A picture of spaghetti"
-					class= "Restaurantfoto">
-					Dutch, European, Modern
+					class= "Restaurantfoto2">
 				</p>
 			</li>
 			<li class="Restaurants1">
 			Urban Frenchy Bistro Toujours
 				<p class="Restaurants">
-					Oude groenmarkt 10-12
+					<h4 class="Adress">Oude groenmarkt 10-12</h4>
+					Dutch, fish and seafood, European
 					<img src="img/Restaurant7.png"
 					title="Urban Frenchy Bistro Toujours"
 					alt= "A picture of salmon"
-					class= "restaurantfoto">
-					Dutch, fish and seafood, European
+					class= "restaurantfoto1">
 				</p>
 			</li>
 			<li class="Restaurants2">
 			The golden bull
 				<p class="Restaurants">
-					Zijlstraat 39
+					<h4 class="Adress">Zijlstraat 39</h4>
+					Steakhouse, Argentinian, European
 					<img src="img/Restaurant8.png"
 					title="The golden bull"
 					alt= "A picture of potatoes and steak"
-					class= "Restaurantfoto">
-					Steakhouse, Argentinian, European
+					class= "Restaurantfoto2">
 				</p>
 			</li>
 		</ol>
