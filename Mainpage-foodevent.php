b8cc7ac84dda7bfdf6ba76fa0b16e234aed474f2
Nicky Garcia Jorge <619307@student.inholland.nl>
2021-03-12 01:25:05 +0100

    WIP: finished restaurant info, buttons nog toevoegen

--- Mainpage-foodevent.php
+++ Mainpage-foodevent.php
@@ -5,8 +5,7 @@
 <html>
 	<body>
 
-		<section class="img-column-foodpage">
-			
+		<section class="img-column-foodpage">	
 		</section>
 
 		<header class="indexHeader">
@@ -14,95 +13,112 @@
 		<h2 class="indexSlogan">Good food is all you need</h2>
 		</header>
 
-		<ol>
-			<li class="Restaurants1">
-			Restaurant MR. & Mrs.
-				<p class="Restaurants">
-					<h4 class="Adress">Lange Veerstraat 4</h4>
-					Dutch, Fish and seafood, European
-					<img src="img/Restaurant1.png "  
+
+		<h1 class="foodFilterButton">Type of food</h1>
+		<section class="filterBar">
+			<input class="foodTypeInput" type="text" name="foodType" placeholder="Or fill in a food type...">
+		</section>
+
+
+		<ul class="Lijst">
+			<li class="Restaurants">
+				
+				<h1 class="Restaurantnaam">Restaurant MR. & Mrs.</h1>
+				<img src="img/Restaurant1.png "  
 					title="Restaurant MR. & Mrs."
 					alt= "A picture of a shrimp"
-					class= "Restaurantfoto1">
+					class= "Restaurantfoto">
+				<p class="RestaurantInfo">
+					Location: Lange Veerstraat 4 
+					<br>
+					<br> Food: Dutch, Fish and seafood, European
 				</p>
 			</li>
-			<li class="Restaurants2">
-			Ratatouille
-				<p class="Restaurants">
-					<h4 class="Adress">Spaarne 96</h4>
-					French, Fish and seafood, European
-					<img src="img/Restaurant2.png"
+			<li class="Restaurants">
+				<h1 class="Restaurantnaam">Ratatouille</h1>
+				<img src="img/Restaurant2.png"
 					title="Ratatouille"
 					alt= "A picture of ratatouille"
-					class= "Restaurantfoto2">
+					class= "Restaurantfoto">
+				<p class="RestaurantInfo">
+					Location: Spaarne 9 
+					<br>
+					<br> Food: French, Fish and seafood, European
 				</p>
 			</li>
-			<li class="Restaurants1">
-			Restaurant ML
-				<p class="Restaurants">
-					<h4 class="Adress">Kleine Houtstraat 70</h4>
-					Dutch, fish and seafood, European
-					<img src="img/Restaurant3.png"
+			<li class="Restaurants">
+				<h1 class="Restaurantnaam">Restaurant ML</h1>			
+				<img src="img/Restaurant3.png"
 					title="Restaurant ML"
 					alt= "A picture of seafood"
-					class= "restaurantfoto1">
+					class= "Restaurantfoto">
+				<p class="RestaurantInfo">
+					Location: Kleine Houtstraat 70 
+					<br>
+					<br> Food: Dutch, fish and seafood, European
 				</p>
 			</li>
-			<li class="Restaurants2">
-			Restaurant Fris
-				<p class="Restaurants">
-					<h4 class="Adress">Twijnderslaan 7</h4>				
-					Dutch, European
-					<img src="img/Restaurant4.png"
+			<li class="Restaurants">
+				<h1 class="Restaurantnaam">Restaurant Fris</h1>
+				<img src="img/Restaurant4.png"
 					title="Restaurant Fris"
 					alt= "A picture of a steak"
-					class= "Restaurantfoto2">
+					class= "Restaurantfoto">
+				<p class="RestaurantInfo">
+					Location: Twijnderslaan 7 
+					<br>
+					<br> Food: Dutch, European
+					<br>
 				</p>
 			</li>
-			<li class="Restaurants1">
-				Specktakel
-				<p class="Restaurants">
-					<h4 class="Adress">Spekstraat 4</h4>
-					European, International, Asian
-					<img src="img/Restaurant5.png"
+			<li class="Restaurants">
+				<h1 class="Restaurantnaam">Specktakel</h1>
+				<img src="img/Restaurant5.png"
 					title="Specktakel"
 					alt= "A picture of an asian dish"
-					class= "restaurantfoto1">
+					class= "Restaurantfoto">
+				<p class="RestaurantInfo">
+					Location: Spekstraat 4 
+					<br>
+					<br> Food: European, International, Asian
 				</p>
 			</li>
-			<li class="Restaurants2">
-			Grand Cafe Brinkman
-				<p class="Restaurants">
-					<h4 class="Adress">Grote markt 13</h4>
-					Dutch, European, Modern
-					<img src="img/Restaurant6.png"
+			<li class="Restaurants">
+			<h1 class="Restaurantnaam">Grand Cafe Brinkman</h1>
+			<img src="img/Restaurant6.png"
 					title="Grand Cafe Brinkman"
 					alt= "A picture of spaghetti"
-					class= "Restaurantfoto2">
+					class= "Restaurantfoto">
+				<p class="RestaurantInfo">
+					Location: Grote markt 13 
+					<br>
+					<br> Food: Dutch, European, Modern
 				</p>
 			</li>
-			<li class="Restaurants1">
-			Urban Frenchy Bistro Toujours
-				<p class="Restaurants">
-					<h4 class="Adress">Oude groenmarkt 10-12</h4>
-					Dutch, fish and seafood, European
-					<img src="img/Restaurant7.png"
+			<li class="Restaurants">
+			<h1 class="Restaurantnaam">Urban Frenchy Bistro Toujours</h1>
+			<img src="img/Restaurant7.png"
 					title="Urban Frenchy Bistro Toujours"
 					alt= "A picture of salmon"
-					class= "restaurantfoto1">
+					class= "Restaurantfoto">
+				<p class="RestaurantInfo">
+					Location: Oude groenmarkt 10-12 
+					<br>
+					<br> Food: Dutch, fish and seafood, European
 				</p>
 			</li>
-			<li class="Restaurants2">
-			The golden bull
-				<p class="Restaurants">
-					<h4 class="Adress">Zijlstraat 39</h4>
-					Steakhouse, Argentinian, European
-					<img src="img/Restaurant8.png"
+			<li class="Restaurants">
+			<h1 class="Restaurantnaam">The golden bull</h1>
+			<img src="img/Restaurant8.png"
 					title="The golden bull"
 					alt= "A picture of potatoes and steak"
-					class= "Restaurantfoto2">
+					class= "Restaurantfoto">
+				<p class="RestaurantInfo">
+					Location: Zijlstraat 39 
+					<br>
+					<br> Food: Steakhouse, Argentinian, European
 				</p>
 			</li>
-		</ol>
+		</ul>
 	</body>
 </html>
