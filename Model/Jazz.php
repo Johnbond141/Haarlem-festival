9c53041679094aba533c110d1c8a643df1008bd8
John <johnbond@live.nl>
2021-03-13 17:56:35 +0100

    WIP: CMS to edit jazz agenda, trying to make popup edit screen

--- Model/Jazz.php
+++ Model/Jazz.php
@@ -11,11 +11,34 @@ class Jazz extends Db
 
             while ($row = mysqli_fetch_assoc($result)) {
                 echo '
+            <section id="overlay">
+                <section class="main">
+                    <p class="sign" align="center">Change performance</p>
+                    <form class="form1">
+                        <input name="Band" class="un" type="text" align="center" value="'.$row['Band'].'">
+                        <input name="Location" class="un" type="text" align="center" value="'.$row['Location'].'">
+                        <input name="Hall" class="un" type="text" align="center" value="'.$row['Hall'].'">
+                        <input name="Time" class="un" type="text" align="center" value="'.$row['Time'].'">
+                        <input name="Date" class="un" type="text" align="center" value="'.$row['Date'].'">
+                        <button class="submit" style="margin-left: 18%; margin-bottom: 5%" name="exit" onclick="overlayOff()">Exit</button>
+                        <button class="submit" align="center" name="save">Save</button>
+                    </form>
+                </section>
+            </section>
             <article class="info-wrapper">
                 <h2>' . $row['Band'] . '</h2>
                 <p>' . $row['Location'] . ', ' . $row['Hall'] . '</p>
                 <p>' . $row['Time'] . ', ' . $row['Date'] . '</p>
+                <img src="img/edit.png" class="edit-icon" name="'.$row['Band'].'" onclick="overlayOn()">
             </article>
+            <script>
+                function overlayOn(){
+                    document.getElementById("overlay").style.display = "block";
+                }
+                function overlayOff(){
+                    document.getElementById("overlay").style.display = "none";
+                }
+            </script>
             ';
             }
 
--- dashboard-jazz.php
+++ dashboard-jazz.php
@@ -18,11 +18,11 @@ include_once 'header.php';
             <section class="column">
                 <h3 class="dashboardButtonText">Edit Pages</h3>
             </section>
+            <a href="jazz-agenda.php">
             <section class="column">
-                <a href="jazz-agenda.php">
                     <h3 class="dashboardButtonText">Edit Program</h3>
-                </a>
             </section>
+            </a>
             <section class="column">
                 <h3 class="dashboardButtonText">Show Invoices</h3>
             </section>
