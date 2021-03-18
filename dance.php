1df4df88d00fbda3e8151bc0a44887856003ddb3
Jordy <jordy.beentjes@gmail.com>
2021-03-18 18:16:39 +0100

    WIP: Created dance event page

---
+++ dance.php
@@ -0,0 +1,19 @@
+<?php
+    include_once 'navBar.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Haarlem Festival Dance event</title>
+  </head>
+  <body class="index">
+
+    <header class="indexHeader">
+      <h1 class="indexTitle">DANCE</h1>
+      <h2 class="indexSlogan">BOOK YOUR TICKETS NOW....</h2>
+    </header>
+
+  </body>
+</html>
