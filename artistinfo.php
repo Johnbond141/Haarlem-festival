5a035a870b33c47c213a1d39b8a1cc93d045671e
Wout de Roy <woutrvz@gmail.com>
2021-03-18 16:04:49 +0100

    WIP: haarlem info page, Finished: 1 jazz agenda
    heb een pagina voor haarlem jazz agenda af gemaakt zodat ik het design heb voor als ik het ga echoen met php en hoef alleen nog 1 andere foto te vinden voor op de haarlem info pagina en dan is die ook klaar omdat de andere foto geen goede kwaliteit was.

--- artistinfo.php
+++ artistinfo.php
@@ -1,3 +1,10 @@
+<?php
+
+    include("includes/navbar.php");
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,15 +23,56 @@
     </header>
     <section class="agenda">
         <article class="title-wrapper">
-            <h1>Festival Agenda</h1>
-            <button>Thursday</button>
+            <h1>Festival <span style="font-weight: bold;">Agenda</span></h1>
+            <form action="" method="POST">
+                <section class="day-buttons">
+            <input type="submit" name="thursday" value="Thursday" class="thursday">
+            <input type="submit" name="friday" value="Friday" class="friday">
+            <input type="submit" name="saturday" value="Saturday" class="saturday">
+            <input type="submit" name="sunday" value="Sunday" class="sunday">
+            </section>
+            </form>
         </article>
         <article class="info-wrapper">
+        <img src="img/jazzagenda.png" alt="picture of dude on saxophone">
+        <section class="perfomance-one">
             <h2>Gumbo Kings</h2>
             <p>Patronaat, Main Hall</p>
             <p>18:00 - 19:00, July 26th</p>
+            </section>
+            <section class="perfomance-two">
+            <h2>Evolve</h2>
+            <p>Patronaat, Main Hall</p>
+            <p>19:30 - 20:30, July 26th</p>
+            </section>
+            <section class="perfomance-three">
+            <h2>Ntjam Rosie</h2>
+            <p>Patronaat, Main Hall</p>
+            <p>21:00 - 22:00, July 26th</p>
+            </section>
+            <section class="perfomance-four">
+            <h2>Wicked Jazz Sounds</h2>
+            <p>Patronaat, Second Hall</p>
+            <p>18:00 - 19:00, July 26th</p>
+            </section>
+            <section class="perfomance-five">
+            <h2>Tom Thomson Assemble</h2>
+            <p>Patronaat, Second Hall</p>
+            <p>19:30 - 20:30, July 26th</p>
+            </section>
+            <section class="perfomance-six">
+            <h2>Jonna Frazer</h2>
+            <p>Patronaat, Second Hall</p>
+            <p>21:00 - 22:00, July 26th</p>
+            </section>
         </article>
     </section>
 </body>
 
 </html>
+
+<?php
+
+    include("includes/footer.php");
+
+?>
---
+++ infopage.php
@@ -0,0 +1,47 @@
+<?php
+
+    include("includes/navBar.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/jazzcss.css">
+    <title>Haarlem info</title>
+</head>
+<body>
+<header class="haarleminfo-header">
+        <h1 class="eventhead">About<br><span style="margin-left: 200px;">Haarlem</span></h1>
+    </header>
+    <section class="haarleminfo-container">
+        <h2>Haarlem</h2>
+        <p>A cozy historic center, famous museums, shops, 
+            restaurants and the beach around the corner: welcome to Haarlem, 
+            the city that has everything. From hidden streets from a bygone era to hip concept stores. From medieval church to waterfront terrace. 
+            From Dutch Masters to French star chefs. From antiques market to pop concert.
+            Fancy an unforgettable day out? 
+            Visit Haarlem and be surprised by the sights, 
+            boutiques and picturesque squares, by the old and contemporary artists, the Burgundian atmosphere and the rich history.
+        </p>
+        <h2>Haarlem Festival</h2>
+        <p>
+        This year, the Haarlem Festival will of course take place again in the beautiful Haarlem.
+        There will be many different jazz and dance events. But of course
+        you can also enjoy a delicious bite to eat in one of the restaurants featured on
+        the food page of this website.
+        </p>
+    </section>  
+    <img class="haarleminfopic1" src="img/haarleminfo1.png" alt="picture of haarlem">
+    <img class="haarleminfopic2" src="img/haarleminfo2.png" alt="second picture of haarlem">
+</body>
+</html>
+
+<?php
+
+    include("includes/footer.php");
+
+?>
